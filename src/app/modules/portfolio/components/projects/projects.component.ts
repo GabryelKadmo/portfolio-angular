3e0a8b176958280e3dee6a89308aa839647acc42
GabryelKadmo <gabryelkadmo@gmail.com>
2024-06-26 15:14:41 -0300

    feat: added project modals and created dialog

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,14 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
-
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject( MatDialog );
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/projects/versaLogo.png',
@@ -56,4 +59,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog( data: IProjects ) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    })
+}
 }
